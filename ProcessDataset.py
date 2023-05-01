770230564310b7d9ec1264d070e5e6a00f6d028c
YunhaoZZ <zhangenochyun@outlook.com>
2023-04-30 19:51:48 -0500

    process data from steam_games.csv

--- ProcessDataset.py
+++ ProcessDataset.py
@@ -1,9 +1,27 @@
 import pandas
 
 def preprocess(path):
-  df = pandas.read_csv(path)
+  fields = ['App ID', 'Tags', 'Positive Reviews','Negative Reviews']
+  df = pandas.read_csv(path, usecols=fields, delimiter=';')
   return df
 
 path="steam_games.csv"
 df=preprocess(path)
-df
+df = df.dropna()
+# print(df['Tags'].head())
+
+# record tags into a dictionary
+tags = {}
+for row in df['Tags']:
+  tag_pairs = row.split(',')
+  for tag_pair in tag_pairs:
+    tag_items = tag_pair.split(':')
+    if tag_items[0] not in tags.keys():
+      tags[tag_items[0]] = 1
+    else:
+      tags[tag_items[0]] += 1
+
+
+print(tags.keys())
+print(max(tags.values()))
+print(min(tags.values()))
