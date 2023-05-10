326595461250fde47dbec80f15a1e80bb9f1f1a4
Orobosa Ogbemudia <55265742+aardig100@users.noreply.github.com>
2023-05-10 13:55:50 -0500

    merge into one model and save load

--- GamePredictor.py
+++ GamePredictor.py
@@ -1,9 +1,12 @@
 # Main File
 from ProcessDataset import getDataset
+from keras.models import Model
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
+from keras.layers import *
+import os.path
 
 df, x, y = getDataset()
 
@@ -34,22 +37,51 @@ y_train, y_test, x_train, x_test = train_test_split(y, x, test_size=0.2, random_
 # print(y_test[-1])
 
 # define the neural network model
-model = keras.Sequential([
-    keras.layers.Dense(500, activation='relu', input_shape=(500,)),
-    keras.layers.Dense(250, activation='relu'),
-    keras.layers.Dense(1, activation='linear')
-])
 
-# compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
+if os.path.isfile("./Test_Model/saved_model.pb"):
+    model=tf.keras.models.load_model("Test_Model")
+else:
+    model = keras.Sequential(
+    [
+        keras.layers.Dense(500, activation='relu', input_shape=(500,)),
+        keras.layers.Dense(250, activation='relu'),
+        #keras.layers.Dense(1, activation='linear')
+        keras.layers.Dense(1,activation=lambda x: tf.nn.elu(x) + 1)
+    ])
+    #mu = tf.layers.dense(inputs=layer, units=1)
+    #sigma = tf.layers.dense(inputs=layer, units=1,activation=lambda x: tf.nn.elu(x) + 1)
 
-# train the model
-model.fit(y_train, x_train, epochs=10, batch_size=32, validation_data=(y_test, x_test))
+    inp = Input((500,))
+    x = Dense(500, activation='relu')(inp)
+    x = Dense(250, activation='relu')(x)
+    x = Dense(250, activation='relu')(x)
+    x = Dense(250, activation='relu')(x)
+    x = Dense(250, activation='relu')(x)
+    mu = Dense(1, activation='linear')(x)
+    sigma = Dense(1,activation=lambda x: tf.nn.elu(x) + 1)(x)
+
+    model = Model(inp, [mu, sigma])
+
+
+    # compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    # train the model
+    model.fit(y_train, x_train, epochs=10, batch_size=32, validation_data=(y_test, x_test))
+
+    model.save("Test_Model")
+
+
 
 # evaluate the model on the testing set
-loss = model.evaluate(y_test, x_test)
+results = model.evaluate(y_test, x_test)
 
-predictresutl = model.predict(y_test)
-print('Test loss:', loss)
+predictresutl,predictresut2 = model.predict(y_test)
+
+print('Test loss:', results)
 print('Test predict:', np.any(predictresutl < 0))
-print('Test predict:', np.asarray(predictresutl).tolist())
+print('Test predict:', np.asarray(predictresutl))
+print('Test predict:', np.asarray(predictresut2))
+
+(x_test, predictresutl)
+accuracy = keras.metrics.accuracy_score(testY,predictY)
